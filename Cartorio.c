f8be403a04cb4ddbe8ea590a4bca204050b29f91
fe3dmaxdev <126424004+fe3dmaxdev@users.noreply.github.com>
2023-02-26 19:12:20 -0300

    Opção de saida
    Incluso no sistema a opção de saída para o usúario

--- Cartorio.c
+++ Cartorio.c
@@ -127,7 +127,8 @@ int main()
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nome\n\n");
+		printf("\t3 - Deletar nome\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: "); //fim do menu
 	
 		scanf("%d", &opcao); //armazenando a escolha do usuario
@@ -148,6 +149,12 @@ int main()
 			case 3:
 			deletar();
 			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;
+			
 						
 			default:
 			printf("Essa opção não esta disponivel!\n");
